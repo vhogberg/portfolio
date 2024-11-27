3a39aaa995dcb04c44ef7d7dd8e65b5448b62850
vhogberg <119532259+vhogberg@users.noreply.github.com>
2024-11-27 19:00:15 +0100

    fixed padding and added button listeners

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+// Navigation
+
+
+
+// Header button listeners
+document.getElementById("header-navigation-button").addEventListener("click", ToDo);
+document.getElementById("header-photography-portfolio-button").addEventListener("click", ToDo);
+
+// Siderbar button listeners
+document.getElementById("sidebar-resume-button").addEventListener("click", ToDo);
+document.getElementById("sidebar-social-github-button").addEventListener("click", ToDo);
+document.getElementById("sidebar-social-linkedin-button").addEventListener("click", ToDo);
+document.getElementById("sidebar-social-email-button").addEventListener("click", ToDo);
+document.getElementById("sidebar-social-instagram-button").addEventListener("click", ToDo);
